1b68aa83c3192cb4e89cbe9a5eef98faa24c42bf
marooo326 <ktnqkr01@gmail.com>
2024-02-09 23:20:57 +0900

    refactor: SecurityUtil로 로직 분리

--- core/core-security/src/main/java/com/pgms/coresecurity/resolver/CurrentAccountArgumentResolver.java
+++ core/core-security/src/main/java/com/pgms/coresecurity/resolver/CurrentAccountArgumentResolver.java
@@ -1,20 +1,13 @@
 package com.pgms.coresecurity.resolver;
 
-import java.util.Objects;
-
 import org.springframework.core.MethodParameter;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import com.pgms.coredomain.domain.common.SecurityErrorCode;
-import com.pgms.coresecurity.exception.SecurityException;
-import com.pgms.coresecurity.user.normal.UserDetailsImpl;
+import com.pgms.coresecurity.util.SecurityUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,15 +27,6 @@ public class CurrentAccountArgumentResolver implements HandlerMethodArgumentReso
 	@Override
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		validateAuthentication(authentication);
-		UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
-		return userDetails.getId();
-	}
-
-	private void validateAuthentication(Authentication authentication) {
-		if (Objects.isNull(authentication) || !(authentication instanceof UsernamePasswordAuthenticationToken)) {
-			throw new SecurityException(SecurityErrorCode.UNAUTHORIZED);
-		}
+		return SecurityUtil.getCurrentAccountId();
 	}
 }
