31972c882486cf6822fbe8cfa35450a56c0d3f7e
park0jae <okvv26@naver.com>
2024-02-13 17:51:28 +0900

    refactor: Member 엔티티 수정

--- core/core-domain/src/main/java/com/pgms/coredomain/domain/member/Member.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/member/Member.java
@@ -3,20 +3,14 @@ package com.pgms.coredomain.domain.member;
 import static com.pgms.coredomain.domain.member.AccountStatus.*;
 
 import com.pgms.coredomain.domain.common.BaseEntity;
-import com.pgms.coredomain.domain.game.GameRoom;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -52,10 +46,6 @@ public class Member extends BaseEntity {
 	@Column(name = "provider")
 	private ProviderType providerType;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "game_room_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-	private GameRoom gameRoom;
-
 	@Builder
 	public Member(String email, String password, String nickname, ProviderType providerType, Role role) {
 		this.email = email;
@@ -76,9 +66,5 @@ public class Member extends BaseEntity {
 
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
-	}
-
-	public void setGameRoom(GameRoom gameRoom) {
-		this.gameRoom = gameRoom;
 	}
 }
