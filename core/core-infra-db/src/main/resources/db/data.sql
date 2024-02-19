ae8a490abb53d5823e02644f5eb063e20d79defd
marooo326 <ktnqkr01@gmail.com>
2024-02-20 00:30:01 +0900

    docs: data.sql & README 수정

--- core/core-infra-db/src/main/resources/db/data.sql
+++ core/core-infra-db/src/main/resources/db/data.sql
@@ -1,23 +1,29 @@
-INSERT INTO member (created_at, email, nickname, password, role, status, updated_at)
-VALUES (NOW(), 'test1@naver.com', '박영재', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK',
-        'ROLE_USER', 'ACTIVE', NOW());
+INSERT INTO member (email, nickname, password, role, status, created_at, updated_at, total_score, average_wpm,
+                    average_accuracy)
+VALUES ('test1@naver.com', '박영재', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK', 'ROLE_USER',
+        'ACTIVE', NOW(), NOW(), 0, 0, 0);
 
-INSERT INTO member (created_at, email, nickname, password, role, status, updated_at)
-VALUES (NOW(), 'test2@naver.com', '김대휘', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK',
-        'ROLE_USER', 'ACTIVE', NOW());
+INSERT INTO member (email, nickname, password, role, status, created_at, updated_at, total_score, average_wpm,
+                    average_accuracy)
+VALUES ('test2@naver.com', '김대휘', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK', 'ROLE_USER',
+        'ACTIVE', NOW(), NOW(), 0, 0, 0);
 
-INSERT INTO member (created_at, email, nickname, password, role, status, updated_at)
-VALUES (NOW(), 'test3@naver.com', '신종욱', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK',
-        'ROLE_USER', 'ACTIVE', NOW());
+INSERT INTO member (email, nickname, password, role, status, created_at, updated_at, total_score, average_wpm,
+                    average_accuracy)
+VALUES ('test3@naver.com', '신종욱', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK', 'ROLE_USER',
+        'ACTIVE', NOW(), NOW(), 0, 0, 0);
 
-INSERT INTO member (created_at, email, nickname, password, role, status, updated_at)
-VALUES (NOW(), 'test4@naver.com', '이예진', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK',
-        'ROLE_USER', 'ACTIVE', NOW());
+INSERT INTO member (email, nickname, password, role, status, created_at, updated_at, total_score, average_wpm,
+                    average_accuracy)
+VALUES ('test4@naver.com', '이예진', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK', 'ROLE_USER',
+        'ACTIVE', NOW(), NOW(), 0, 0, 0);
 
-INSERT INTO member (created_at, email, nickname, password, role, status, updated_at)
-VALUES (NOW(), 'test5@naver.com', '김영현', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK',
-        'ROLE_USER', 'ACTIVE', NOW());
+INSERT INTO member (email, nickname, password, role, status, created_at, updated_at, total_score, average_wpm,
+                    average_accuracy)
+VALUES ('test5@naver.com', '김영현', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK', 'ROLE_USER',
+        'ACTIVE', NOW(), NOW(), 0, 0, 0);
 
-INSERT INTO member (created_at, email, nickname, password, role, status, updated_at)
-VALUES (NOW(), 'test6@naver.com', '백준원', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK',
-        'ROLE_USER', 'ACTIVE', NOW());
+INSERT INTO member (email, nickname, password, role, status, created_at, updated_at, total_score, average_wpm,
+                    average_accuracy)
+VALUES ('test6@naver.com', '백준원', '{bcrypt}$2a$10$s7ZyDaubS0bZejb6Z4TmnOEBwU/b7k0gSKrdnAdejw0eiiPGsgNoK', 'ROLE_USER',
+        'ACTIVE', NOW(), NOW(), 0, 0, 0);
