5f5a265e089005f3a9e2da1072fa8843e403c162
marooo326 <ktnqkr01@gmail.com>
2024-02-20 16:36:33 +0900

    refactor: 웹소켓 연결 엔드포인트 변경

--- api/src/main/java/com/pgms/api/socket/config/WebSocketConfig.java
+++ api/src/main/java/com/pgms/api/socket/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws/room")
+		registry.addEndpoint("/ws")
 			.setAllowedOriginPatterns("*")
 			.withSockJS();
 	}
