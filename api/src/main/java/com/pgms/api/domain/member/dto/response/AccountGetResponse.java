7a7d1381a8eb04ebcdd8a99fd879c0f6f9d44fc0
marooo326 <ktnqkr01@gmail.com>
2024-02-27 10:45:59 +0900

    refactor: AccountGetResponse 필드이름 변경

--- api/src/main/java/com/pgms/api/domain/member/dto/response/AccountGetResponse.java
+++ api/src/main/java/com/pgms/api/domain/member/dto/response/AccountGetResponse.java
@@ -5,9 +5,9 @@ import com.pgms.coreinfraredis.entity.Guest;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(title = "프로필 응답", requiredProperties = {"accountId", "email", "nickname", "status"})
+@Schema(title = "프로필 응답", requiredProperties = {"memberId", "nickname", "isGuest"})
 public record AccountGetResponse(
-	Long accountId,
+	Long memberId,
 	String email,
 	String nickname,
 	boolean isGuest
