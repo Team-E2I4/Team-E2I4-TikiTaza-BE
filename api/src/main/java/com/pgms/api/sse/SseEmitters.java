27d789135865b43aeb7314f32f36f8293fdf5db3
park0jae <okvv26@naver.com>
2024-02-27 15:42:13 +0900

    fix: SSE 메세지 비동기 처리 및 동시성 문제 해결

--- api/src/main/java/com/pgms/api/sse/SseEmitters.java
+++ api/src/main/java/com/pgms/api/sse/SseEmitters.java
@@ -1,8 +1,10 @@
 package com.pgms.api.sse;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -13,26 +15,19 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class SseEmitters {
 
-	private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+	private final ConcurrentHashMap<SseEmitter, Boolean> emitters = new ConcurrentHashMap<>();
+	private final ExecutorService executorService = Executors.newCachedThreadPool();
 
 	// SSE Emitter 등록 메소드
 	public void addEmitter(SseEmitter emitter) {
-		this.emitters.add(emitter);
+		this.emitters.put(emitter, true);
 		log.info(">>>>>> SSE Emitter Add {}", emitter);
 
 		// Broken Pipe 발생 시 연결 종료 및 리소스 정리
-		emitter.onError(throwable -> {
-			log.warn(">>>>>> SSE Emitter on Error");
-			emitter.complete();
-			this.emitters.remove(emitter);
-		});
+		emitter.onError(throwable -> handleEmitterError(emitter));
 
 		// Time-Out 발생 시 연결 종료 및 리소스 정리
-		emitter.onTimeout(() -> {
-			log.warn(">>>>>> SSE Emitter Timeout");
-			emitter.complete();
-			this.emitters.remove(emitter);
-		});
+		emitter.onTimeout(() -> handleEmitterError(emitter));
 
 		// 비동기 요청 완료시 emitter 객체 삭제
 		emitter.onCompletion(() -> {
@@ -48,17 +43,17 @@ public class SseEmitters {
 
 	public void updateGameRoom(Object roomInfo) {
 		log.warn(">>>>>> SSE Emitter Change GameRoom");
-		emitters.forEach(emitter -> {
-			try {
-				emitter.send(SseEmitter.event()
-					.name("changeGameRoom")
-					.data(roomInfo));
-			} catch (IOException e) {
-				log.warn(">>>>>> SSE Emitter Change GameRoom Exception");
-				emitter.complete();
-				this.emitters.remove(emitter);
-			}
-		});
+		for (SseEmitter emitter : emitters.keySet()) {
+			CompletableFuture.runAsync(() -> {
+				try {
+					emitter.send(SseEmitter.event()
+						.name("changeGameRoom")
+						.data(roomInfo));
+				} catch (IOException e) {
+					handleEmitterError(emitter);
+				}
+			}, executorService);
+		}
 	}
 
 	public void connect(SseEmitter emitter, Object roomInfo) {
@@ -67,8 +62,13 @@ public class SseEmitters {
 				.name("connect")
 				.data(roomInfo));
 		} catch (IOException e) {
-			emitter.complete(); // 예외 발생 시 emitter를 종료
-			this.emitters.remove(emitter);
+			handleEmitterError(emitter);
 		}
+	}
+
+	private void handleEmitterError(SseEmitter emitter) {
+		log.warn(">>>>>> SSE Emitter Error");
+		emitter.complete();
+		this.emitters.remove(emitter);
 	}
 }
