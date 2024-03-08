c2a96a1be95def1ac71b28755191c2e94d3407b9
marooo326 <ktnqkr01@gmail.com>
2024-03-08 13:23:59 +0900

    fix: GameQuestion 스키마 변경, 기본 데이터 추가

--- api/src/main/resources/db/data.sql
+++ api/src/main/resources/db/data.sql
@@ -1,8 +1,7 @@
 insert into member (created_at, updated_at, email, nickname, password, role, status)
 values (NOW(), NOW(), 'test1@naver.com', '박영재', '{bcrypt}$2a$10$jSTH3yTOzqXr14LYOz.BJ.gCjCvDOeTKaNoJ2/qyxlFXB1ii7r8dC',
-        'ROLE_USER', 'ACTIVE');
-insert into member (created_at, updated_at, email, nickname, password, role, status)
-values (NOW(), NOW(), 'test2@naver.com', '김대휘', '{bcrypt}$2a$10$jSTH3yTOzqXr14LYOz.BJ.gCjCvDOeTKaNoJ2/qyxlFXB1ii7r8dC',
+        'ROLE_USER', 'ACTIVE'),
+       (NOW(), NOW(), 'test2@naver.com', '김대휘', '{bcrypt}$2a$10$jSTH3yTOzqXr14LYOz.BJ.gCjCvDOeTKaNoJ2/qyxlFXB1ii7r8dC',
         'ROLE_USER', 'ACTIVE');
 
 INSERT INTO game_question (id, question, game_type)
@@ -170,3 +169,60 @@ VALUES (1, '사과1', 'WORD'),
 -- 추가 필요한 만큼 중복되지 않는 단어를 추가합니다.
        (149, '복구', 'WORD'),
        (150, '시스템관리', 'WORD');
+
+INSERT INTO game_question (created_at, updated_at, question, game_type)
+VALUES (NOW(), NOW(),
+        'public class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello, World!");\n\t}\n}',
+        'CODE'),
+       (NOW(), NOW(),
+        'public class AddTwoNumbers {\n\tpublic static void main(String[] args) {\n\t\tint num1 = 5;\n\t\tint num2 = 10;\n\t\tint sum = num1 + num2;\n\t\tSystem.out.println("Sum: " + sum);\n\t}\n}',
+        'CODE'),
+       (NOW(), NOW(),
+        'import java.util.Scanner;\n\npublic class UserInput {\n\tpublic static void main(String[] args) {\n\t\tScanner scanner = new Scanner(System.in);\n\t\tSystem.out.print("Enter a number: ");\n\t\tint userInput = scanner.nextInt();\n\t\tSystem.out.println("You entered: " + userInput);\n\t}\n}',
+        'CODE'),
+       (NOW(), NOW(),
+        'public class LoopExample {\n\tpublic static void main(String[] args) {\n\t\tfor (int i = 1; i <= 10; i++) {\n\t\t\tSystem.out.println(i);\n\t\t}\n\t}\n}',
+        'CODE'),
+       (NOW(), NOW(),
+        'import java.util.Scanner;\n\npublic class PositiveNegativeZero {\n\tpublic static void main(String[] args) {\n\t\tScanner scanner = new Scanner(System.in);\n\t\tSystem.out.print("Enter a number: ");\n\t\tint number = scanner.nextInt();\n\n\t\tif (number > 0) {\n\t\t\tSystem.out.println("Positive");\n\t\t} else if (number < 0) {\n\t\t\tSystem.out.println("Negative");\n\t\t} else {\n\t\t\tSystem.out.println("Zero");\n\t\t}\n\t}\n}',
+        'CODE'),
+       (NOW(), NOW(), 'console.log("Hello, World!");', 'CODE'),
+       (NOW(), NOW(), 'let num1 = 5;\nlet num2 = 10;\nlet sum = num1 + num2;\nconsole.log("Sum:", sum);', 'CODE'),
+       (NOW(), NOW(), 'let userInput = prompt("Enter a number:");\nconsole.log("You entered:", userInput);', 'CODE'),
+       (NOW(), NOW(), 'for (let i = 1; i <= 10; i++) {\n\tconsole.log(i);\n}', 'CODE'),
+       (NOW(), NOW(),
+        'let number = prompt("Enter a number:");\nif (number > 0) {\n\tconsole.log("Positive");\n} else if (number < 0) {\n\tconsole.log("Negative");\n} else {\n\tconsole.log("Zero");\n}',
+        'CODE');
+
+
+INSERT INTO tikitaza.game_question (created_at, updated_at, question, game_type)
+VALUES (NOW(), NOW(), '삶이 있는 한 희망은 있다 -키케로', 'SENTENCE'),
+       (NOW(), NOW(), '산다는것 그것은 치열한 전투이다. -로망로랑', 'SENTENCE'),
+       (NOW(), NOW(), '하루에 3시간을 걸으면 7년 후에 지구를 한바퀴 돌 수 있다. -사무엘존슨', 'SENTENCE'),
+       (NOW(), NOW(), '언제나 현재에 집중할수 있다면 행복할것이다. -파울로 코엘료', 'SENTENCE'),
+       (NOW(), NOW(), '진정으로 웃으려면 고통을 참아야하며, 나아가 고통을 즐길 줄 알아야 해 -찰리 채플린', 'SENTENCE'),
+       (NOW(), NOW(), '직업에서 행복을 찾아라. 아니면 행복이 무엇인지 절대 모를 것이다 -엘버트 허버드', 'SENTENCE'),
+       (NOW(), NOW(), '신은 용기있는자를 결코 버리지 않는다 -켄러', 'SENTENCE'),
+       (NOW(), NOW(), '행복의 문이 하나 닫히면 다른 문이 열린다. 그러나 우리는 종종 닫힌 문을 멍하니 바라보다가', 'SENTENCE'),
+       (NOW(), NOW(), '우리를 향해 열린 문을 보지 못하게 된다 – 헬렌켈러', 'SENTENCE'),
+       (NOW(), NOW(), '피할수 없으면 즐겨라 – 로버트 엘리엇', 'SENTENCE'),
+       (NOW(), NOW(), '단순하게 살아라. 현대인은 쓸데없는 절차와 일 때문에 얼마나 복잡한 삶을 살아가는가? -이드리스 샤흐', 'SENTENCE'),
+       (NOW(), NOW(), '먼저 자신을 비웃어라. 다른 사람이 당신을 비웃기 전에 – 엘사 맥스웰', 'SENTENCE'),
+       (NOW(), NOW(), '먼저핀꽃은 먼저진다. 남보다 먼저 공을 세우려고 조급히 서둘것이 아니다 – 채근담', 'SENTENCE'),
+       (NOW(), NOW(), '행복한 삶을 살기위해 필요한 것은 거의 없다. -마르쿠스 아우렐리우스 안토니우스', 'SENTENCE'),
+       (NOW(), NOW(), '절대 어제를 후회하지 마라. 인생은 오늘의 나 안에 있고 내일은 스스로 만드는 것이다 L.론허바드', 'SENTENCE'),
+       (NOW(), NOW(), '어리석은 자는 멀리서 행복을 찾고, 현명한 자는 자신의 발치에서 행복을 키워간다 -제임스 오펜하임', 'SENTENCE'),
+       (NOW(), NOW(), '너무 소심하고 까다롭게 자신의 행동을 고민하지 말라. 모든 인생은 실험이다. 더 많이 실험할수록 더 나아진다 – 랄프 왈도 에머슨', 'SENTENCE'),
+       (NOW(), NOW(), '한번의 실패와 영원한 실패를 혼동하지 마라 -F.스콧 핏제랄드', 'SENTENCE'),
+       (NOW(), NOW(), '내일은 내일의 태양이 뜬다', 'SENTENCE'),
+       (NOW(), NOW(), '피할수 없으면 즐겨라 -로버트 엘리엇', 'SENTENCE'),
+       (NOW(), NOW(), '절대 어제를 후회하지 마라. 인생은 오늘의 내 안에 있고 내일은 스스로 만드는것이다. -L론허바드', 'SENTENCE'),
+       (NOW(), NOW(), '계단을 밟아야 계단 위에 올라설수 있다, -터키속담', 'SENTENCE'),
+       (NOW(), NOW(), '오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다, -앙드레 말로', 'SENTENCE'),
+       (NOW(), NOW(), '좋은 성과를 얻으려면 한 걸음 한 걸음이 힘차고 충실하지 않으면 안 된다, -단테', 'SENTENCE'),
+       (NOW(), NOW(), '행복은 습관이다, 그것을 몸에 지니라 -허버드', 'SENTENCE'),
+       (NOW(), NOW(), '성공의 비결은 단 한 가지, 잘할 수 있는 일에 광적으로 집중하는 것이다.- 톰 모나건', 'SENTENCE'),
+       (NOW(), NOW(), '자신감 있는 표정을 지으면 자신감이 생긴다 -찰스다윈', 'SENTENCE'),
+       (NOW(), NOW(), '평생 살 것처럼 꿈을 꾸어라. 그리고 내일 죽을 것처럼 오늘을 살아라. – 제임스 딘', 'SENTENCE'),
+       (NOW(), NOW(), '네 믿음은 네 생각이 된다 . 네 생각은 네 말이 된다. 네말은 네 행동이 된다 네 행동은 네 습관이 된다. 네 습관은 네 가치가 된다 . 네 가치는 네 운명이 된다',
+        'SENTENCE');
--- core/core-domain/src/main/java/com/pgms/coredomain/domain/game/GameQuestion.java
+++ core/core-domain/src/main/java/com/pgms/coredomain/domain/game/GameQuestion.java
@@ -23,7 +23,7 @@ public class GameQuestion extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "question")
+	@Column(name = "question", nullable = false, length = 5000)
 	private String question;
 
 	@Column(name = "game_type")
