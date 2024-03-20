f419c11a8f23bbfd2154637276f0ae0af9f7411e
park0jae <okvv26@naver.com>
2024-03-20 21:49:29 +0900

    feat: 온라인 유저 닉네임 반영 로직 재구현

--- api/src/main/java/com/pgms/api/domain/online/service/OnlineService.java
+++ api/src/main/java/com/pgms/api/domain/online/service/OnlineService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.pgms.api.domain.online.dto.OnlineMemberGetResponse;
 import com.pgms.coredomain.repository.GameRoomMemberRepository;
+import com.pgms.coredomain.repository.MemberRepository;
 import com.pgms.coreinfraredis.entity.OnlineMember;
 import com.pgms.coreinfraredis.repository.OnlineMemberRepository;
 import com.pgms.coresecurity.resolver.Account;
@@ -19,17 +20,40 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OnlineService {
 
+	private final MemberRepository memberRepository;
 	private final GameRoomMemberRepository gameRoomMemberRepository;
 	private final OnlineMemberRepository onlineMemberRepository;
 
 	public Set<OnlineMemberGetResponse> getOnlineMembers(Account account) {
-		OnlineMember currentMember = new OnlineMember(account.id(), account.nickname());
-		onlineMemberRepository.save(currentMember);
 		Set<OnlineMemberGetResponse> onlineMembers = new HashSet<>();
+
+		if (!account.isGuest()) {
+			addOnlineMemberForMember(account, onlineMembers);
+		} else {
+			addOnlineMemberForGuest(account, onlineMembers);
+		}
 		addGameRoomMembers(onlineMembers);
 		addOnlineMembers(onlineMembers);
 
 		return onlineMembers;
+	}
+
+	private void addOnlineMemberForMember(Account account, Set<OnlineMemberGetResponse> onlineMembers) {
+		memberRepository.findById(account.id())
+			.ifPresent(currentMember -> saveAndAddToOnlineMembers(
+				account.id(),
+				currentMember.getNickname(),
+				onlineMembers));
+	}
+
+	private void addOnlineMemberForGuest(Account account, Set<OnlineMemberGetResponse> onlineMembers) {
+		saveAndAddToOnlineMembers(account.id(), account.nickname(), onlineMembers);
+	}
+
+	private void saveAndAddToOnlineMembers(Long memberId, String nickname, Set<OnlineMemberGetResponse> onlineMembers) {
+		OnlineMember onlineMember = new OnlineMember(memberId, nickname);
+		onlineMemberRepository.save(onlineMember);
+		onlineMembers.add(new OnlineMemberGetResponse(onlineMember.getId(), onlineMember.getNickname()));
 	}
 
 	private void addGameRoomMembers(Set<OnlineMemberGetResponse> onlineMembers) {
