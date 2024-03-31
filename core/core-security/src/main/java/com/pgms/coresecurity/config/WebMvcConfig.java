5f584c5d79f93c9225b8aad13618f039a221dc61
marooo326 <ktnqkr01@gmail.com>
2024-04-01 00:09:20 +0900

    chore: CORS 설정 변경

--- core/core-security/src/main/java/com/pgms/coresecurity/config/WebMvcConfig.java
+++ core/core-security/src/main/java/com/pgms/coresecurity/config/WebMvcConfig.java
@@ -37,7 +37,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 			"http://localhost:5173",
 			"https://localhost:5173",
 			"https://api.tikitaza.com",
-			"https://tikitaza.vercel.app",
+			"https://tikitaza.com",
 			"https://www.tikitaza.com"
 		).toArray(String[]::new);
 	}
